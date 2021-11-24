cd900440e514dbc02c095fbf6331afc30cd8ba41
mdaabis <mostafadaabis@gmail.com>
2021-11-24 17:06:30 +0000

    Add getAverageWordLength(), getMostFrequentlyOccuringWordLength() and cleanup.

--- src/main/java/WordCounter.java
+++ src/main/java/WordCounter.java
@@ -1,34 +1,66 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class WordCounter {
 	
 	public static final String FILE_PATH = "C:\\Users\\mosta\\OneDrive\\Documents\\words.txt";
 	
+//	TODO check if methods should be static
+	
 	public static void main(String[] args) {
-		
+	    String text = convertFileToString();
 	}
 	
-	public int getTotalWords() {	
-	    String data = convertFileToString();
-	    String[] words = data.split(" ");
+	public static int getTotalWords(String text) {	
+		if(text.equals(""))
+			return 0;
+	    String[] words = text.split(" ");
 	    return words.length;
 	}
 	
-	public int getTotalCharactersNotSpaces() {
-	    String text = convertFileToString();
-	    text = removeSpaces(text);
-	    String[] words = text.split("");
+	public static int getTotalCharactersExcludingSpaces(String text) {
+	    String textWithoutSpaces = removeSpaces(text);
+	    String[] words = textWithoutSpaces.split("");
 
 	    return words.length;
 	}
 	
-	public String removeSpaces(String text) {
+	public static double getAverageWordLength(String text) {
+		if(getTotalWords(text) == 0)
+			return 0.0;
+		return getTotalCharactersExcludingSpaces(text) / getTotalWords(text);
+	}
+	
+	public static int getMostFrequentlyOccuringWordLength(String text) {
+	    String[] words = text.split(" ");
+		Map<Integer, Integer> frequencyMap = new TreeMap<>();
+		
+		for(String word : words) {
+			if(!frequencyMap.containsKey(word.length()))
+				frequencyMap.put(word.length(), 0);
+				
+			frequencyMap.put(word.length(), frequencyMap.get(word.length()) + 1);
+		}
+		
+		List<Map.Entry<Integer, Integer>> entryList = new ArrayList<>(frequencyMap.entrySet());
+
+		Collections.sort(entryList, (e1, e2) -> e2.getValue().compareTo(e1.getValue()));
+
+		return entryList.get(0).getValue();
+
+	}
+	
+	public static String removeSpaces(String text) {
 		return text.replaceAll("\\s+","");
 	}
 	
-	public String convertFileToString() {
+	public static String convertFileToString() {
 		String text = null;
 		
 		try {
@@ -39,6 +71,8 @@ public class WordCounter {
 		
 		return text;
 	}
+	
+
 		
 
 }
--- src/test/java/WordCounterTest.java
+++ src/test/java/WordCounterTest.java
@@ -5,21 +5,51 @@ import org.junit.Test;
 
 public class WordCounterTest {
 	
-	private WordCounter wc;
+	private WordCounter wordCounter;
+	private String data;
+	
 	
 	@Before
 	public void setup() {
-		wc = new WordCounter();
+		wordCounter = new WordCounter();
+	    data = wordCounter.convertFileToString();
 	}
+	
 
 	@Test
+	public void removeSpacesTest() {
+		assertEquals("TextWithoutSpaces", wordCounter.removeSpaces("Text Without Spaces"));
+	}
+	
+	@Test
 	public void getTotalWordsTest() {
-		assertEquals(12, wc.getTotalWords());
+		assertEquals(21, wordCounter.getTotalWords(data));
+		assertEquals(0, wordCounter.getTotalWords(emptyString()));
 	}
 	
 	@Test
 	public void getTotalCharactersNotSpacesTest() {
-		assertEquals(36, wc.getTotalCharactersNotSpaces());
+		assertEquals(69, wordCounter.getTotalCharactersExcludingSpaces(data));
+		assertEquals(0, wordCounter.getTotalWords(emptyString()));
+	}
+	
+	@Test
+	public void getAverageWordLengthTest() {		
+		assertEquals(3, wordCounter.getAverageWordLength(data), 0.001);
+		assertEquals(0, wordCounter.getTotalWords(emptyString()));
+
+	}
+	
+	public String emptyString() {
+		return "";
+	}
+	
+	public String stringWithNumbersAndSpaces() {
+		return "12abc 45d 2 4 d"; 
+	}
+	
+	public String stringWithNumbersAndNoSpaces() {
+		return "12abc45d24d"; 
 	}
 
 }
