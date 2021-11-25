22c97411dd1ec185f5fb76f401de177ebce4fbcc
mdaabis <mostafadaabis@gmail.com>
2021-11-25 17:01:28 +0000

    Match output formatting to example in task specification + add Javadocs.

--- src/main/java/WordCounter.java
+++ src/main/java/WordCounter.java
@@ -9,6 +9,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.stream.Collectors;
 
 public class WordCounter {
 	
@@ -17,9 +18,10 @@ public class WordCounter {
 //	TODO check if methods should be static
 	
 	public static void main(String[] args) {
-	    String text = convertFileToString();
-//	    String text = "Hello world & good morning. The date is 18/05/2016";
+//	    String text = convertFileToString();
+	    String text = "Hello world & good morning. The date is 18/05/2016";
 	    text = cleanText(text);
+	    
 	    int wordCount = getTotalWords(text);
 	    double averageWordLength = getAverageWordLength(text);
 	    List<Map.Entry<Integer, Integer>> lengthFrequencyMap = getWordLengthFrequency(text, true);
@@ -30,9 +32,22 @@ public class WordCounter {
 	    for(Map.Entry<Integer, Integer> entry : lengthFrequencyMap) {
 		    System.out.println("Number of words of length " + entry.getKey() + " is " + entry.getValue());
 	    }
-	    System.out.println("The most frequently occurring word length is " + mostFrequentLengths.get(0).getKey() + ", for word lengths " + mostFrequentLengths.get(0).getValue());
+	    System.out.println("The most frequently occurring word length is " + mostFrequentLengths.get(0).getKey() + ", for word lengths " + 
+	    		mostFrequentLengths
+    				.get(0)
+    				.getValue()
+    				.stream()
+    				.map(String::valueOf)
+    				.collect(Collectors.joining(" & ")));
 	}
 	
+	/**
+	 * Returns the total word count of the string passed. Returns a length of 0 if the 
+	 * argument is an empty string.
+	 * 
+	 * @param text string to be counted
+	 * @return word count
+	 */
 	public static int getTotalWords(String text) {	
 		if(text.equals(""))
 			return 0;
@@ -54,6 +69,19 @@ public class WordCounter {
 		double average = Double.parseDouble(df.format((double) getTotalCharactersExcludingSpaces(text) / getTotalWords(text))); 
 		return average;
 	}
+	
+	/**
+	 * Counts number of occurrences for each word length.
+	 * <p>
+	 * Each word's length is counted and added to frequencyMap if it's not there already. If it is, then 
+	 * the count is incremented. Then, the map is sorted in ascending order either using the key or the value.
+	 * <p>
+	 * 
+	 * @param text string being tested
+	 * @param sortByKey determines whether map is sorted by key (used for printing output) or value (used for
+	 * getting most frequently occurring length)
+	 * @return a map of entries containing word lengths and their frequency
+	 */
 	
 	public static List<Map.Entry<Integer, Integer>> getWordLengthFrequency(String text, boolean sortByKey) {
 	    String[] words = text.split(" ");
@@ -77,10 +105,16 @@ public class WordCounter {
 
 	}
 	
+	/**
+	 * Finds the most frequently occurring word length (or lengths if more than one) by iterating through frequencyList
+	 * 
+	 * @param text string being tested
+	 * @return map with one entry containing maximum occurring word length (key) and a list of all the word lengths with that
+	 * frequency (value)
+	 */
 	public static Map<Integer, List<Integer>> getMostFrequentlyOccuringWordLengths(String text) {
 		List<Map.Entry<Integer, Integer>> frequencyList = getWordLengthFrequency(text, false);
 		int mostFrequentLengthCount = frequencyList.get(frequencyList.size() - 1).getValue();
-		
 		Map<Integer, List<Integer>> maxFrequencies = new HashMap<>();
 		maxFrequencies.put(mostFrequentLengthCount, new ArrayList<>());
 		
@@ -94,13 +128,23 @@ public class WordCounter {
 		return maxFrequencies;
 	}
 	
+	/**
+	 * Remove spaces in a string.
+	 * 
+	 * @param text string with spaces
+	 * @return string with spaces removed
+	 */
 	public static String removeSpaces(String text) {
 		return text.replaceAll("\\s+","");
 	}
 	
+	/**
+	 * Converts input file to string
+	 * 
+	 * @return string version of input file
+	 */
 	public static String convertFileToString() {
 		String text = null;
-		
 		try {
 			text = new String(Files.readAllBytes(Paths.get(FILE_PATH)));
 		} catch (IOException e) {
@@ -110,6 +154,12 @@ public class WordCounter {
 		return text;
 	}
 	
+	/**
+	 * Removes characters we do not want as part of the character/word count.
+	 * 
+	 * @param text string to be cleaned
+	 * @return cleaned text
+	 */
 	public static String cleanText(String text) {
 		text = text.replaceAll("\\.", "");
 		text = text.replaceAll("\\,", "");
